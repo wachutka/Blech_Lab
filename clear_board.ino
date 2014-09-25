39534071bb595ba5c3b8cac27fa62e36e7873098
wachutka <wachutka@brandeis.edu>
2014-09-25 16:08:18 -0400

    Create clear_board.ino

---
+++ clear_board.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  for (int thisPin = 2; thisPin < 14; thisPin++) {
+    pinMode(thisPin, OUTPUT);
+  }
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
