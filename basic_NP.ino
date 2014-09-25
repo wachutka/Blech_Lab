4142c4bda9227f638a265b8929b86b09bc37c55e
wachutka <wachutka@brandeis.edu>
2014-09-25 18:20:28 -0400

    Update basic_NP.ino

--- basic_NP.ino
+++ basic_NP.ino
@@ -8,15 +8,18 @@
   
 int digitalPin1 = 2;     // output pin 1
 int digitalPin2 = 3;     // output pin 2
-int openTime1 = 9;       // time to leave valve 1 open in ms
-int openTime2 = 8;       // time to leave valve 2 open in ms
+int openTime1 = 11;       // time to leave valve 1 open in ms
+int openTime2 = 10;       // time to leave valve 2 open in ms
 int iti1 = 1000;         // first iti in ms
 int iti2 = 3000;         // second iti in ms
-int trials1 = 4;         // number of trials for first iti
-int trials2 = 8;         // total number of trials
+int toneiti = 500;      // tone duration in ms
+int trials1 = 50;         // number of trials for first iti
+int trials2 = 100;         // total number of trials
 
 int digitalPin3 = 4;     // input pin 1
 int digitalPin4 = 5;     // input pin 2
+
+int digitalPin5 = 6;    // output pin 3 for tone
 
 void setup() {
   //start serial connection
@@ -24,17 +27,25 @@ void setup() {
   //configure pin2 and pin3 as outputs
    pinMode(digitalPin1, OUTPUT);
    pinMode(digitalPin2, OUTPUT);
+   pinMode(digitalPin5, OUTPUT);
+   digitalWrite(digitalPin1, LOW);
+   digitalWrite(digitalPin2, LOW);
+   digitalWrite(digitalPin5, LOW);
    pinMode(digitalPin3, INPUT_PULLUP);
    pinMode(digitalPin4, INPUT_PULLUP);
+
    
    for (int trial = 1; trial <= trials1;) {
-    if (digitalRead(digitalPin3) == LOW) {
+     if (digitalRead(digitalPin3) == LOW) {
       digitalWrite(digitalPin1, HIGH);
       delay(openTime1);
       digitalWrite(digitalPin1, LOW);
       Serial.println("Completed trial number"); 
       Serial.println(trial);
-      delay(iti1); 
+      delay(iti1);
+      digitalWrite(digitalPin5, HIGH);
+      delay(toneiti);
+      digitalWrite(digitalPin5, LOW);
       trial ++;
     }
     else if(digitalRead(digitalPin4) == LOW) {
@@ -44,10 +55,16 @@ void setup() {
       Serial.println("Completed trial number"); 
       Serial.println(trial); 
       delay(iti1);
+      digitalWrite(digitalPin5, HIGH);
+      delay(toneiti);
+      digitalWrite(digitalPin5, LOW);
       trial ++;
     }
   }
   for (int trial = trials1 +1; trial <= trials2;) {
+    digitalWrite(digitalPin5, HIGH);
+    delay(toneiti);
+    digitalWrite(digitalPin5, LOW);
     if (digitalRead(digitalPin3) == LOW) {
       digitalWrite(digitalPin1, HIGH);
       delay(openTime1);
@@ -55,6 +72,9 @@ void setup() {
       delay(iti2);
       Serial.println("Completed trial number"); 
       Serial.println(trial); 
+      digitalWrite(digitalPin5, HIGH);
+      delay(toneiti);
+      digitalWrite(digitalPin5, LOW);
       trial ++;
     }
     else if(digitalRead(digitalPin4) == LOW) {
@@ -64,6 +84,10 @@ void setup() {
       delay(iti2);
       Serial.println("Completed trial number"); 
       Serial.println(trial); 
+      delay(iti2);
+      digitalWrite(digitalPin5, HIGH);
+      delay(toneiti);
+      digitalWrite(digitalPin5, LOW);
       trial ++;
     }
   }
