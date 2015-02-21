96234c01e14743688cf16b8b35bc1de48ce17d5c
wachutka <wachutka@brandeis.edu>
2015-02-21 12:21:15 -0500

    Update blech_gonogo.py

--- blech_gonogo.py
+++ blech_gonogo.py
@@ -44,91 +44,121 @@ def clear_tastes(tastes = ['Y1', 'Y2', 'Y3', 'Y4'], duration = 10000):
 
 	print('The purge is complete.  This has been the most sucessful purge yet.')
 
-# Basic nose poke task with 1 poke
-
-def basic_np(outport = 'Y2', opentime = 11, trials = 100, iti = [5000, 10000], resptime = [20000, 15000]):
-
-	inport = 'X7'		# port connected to nose poke 1
-	i = 1			# trial counter
-	ii = 0		# lights makes sure that the lights turn on at the start of the first trial.
-	nopoke = 0
-	
-	pyb.delay(5000)
+# Water passive habituation
+				
+def passive_water(outport = 'Y2', opentime = 11, trials = 40, iti = 15000):
+	pyb.delay(10000)  # start delay
+	i = 1	# trial counter
 	while i <= trials:
-		if i <= (trials/2): # use diff times for the first half of the experiment
+		pyb.Pin(outport, pyb.Pin.OUT_PP).high()
+		pyb.delay(opentime)
+		pyb.Pin(outport, pyb.Pin.OUT_PP).low()
+		print('Trial '+str(i)+' of '+str(trials)+' completed.')
+		i = i+1
+		pyb.delay(iti)
+	print('Shrek says: It\'s all ogre now.')
+
+# Basic nose poke task with 1 pokes
+
+def basic_np(outport = 'Y2', opentime = 13, pokeport = 'X8', trials = 100, iti = [500, 2000,4000], outtime = [250,250]):
+	i = 1			# trial counter
+	ii = 0
+	nopoke = 0
+	light = pyb.Pin('X9', pyb.Pin.OUT_PP)
+	water = pyb.Pin(outport, pyb.Pin.OUT_PP)
+	poke = pyb.Pin(pokeport, pyb.Pin.IN, pyb.Pin.PULL_UP)
+	pyb.delay(10000)
+	while i <= trials:
+		if i <= 5:
 			time1 = pyb.millis()
 			if i - ii >= 1.0:
 				ii = i
 				time2 = pyb.millis()
-				pyb.Pin('X9', pyb.Pin.OUT_PP).high()
-			if pyb.Pin(inport, pyb.Pin.IN).value() == 0: #or pyb.Pin(inport_2, pyb.Pin.IN).value() == 0:
-				pyb.Pin(outport, pyb.Pin.OUT_PP).high()
+				light.high()
+				pyb.delay(5)
+			if poke.value() == 0:
+				time3 = pyb.millis()
+				time4 = pyb.millis()
+				time5 = pyb.millis()
+				while (time4 - time3) < outtime[0]:
+					if poke.value() == 0:
+						time3 = pyb.millis()
+					time4 = pyb.millis()
+				water.high()
 				pyb.delay(opentime)
-				pyb.Pin(outport, pyb.Pin.OUT_PP).low()
-				pyb.Pin('X9', pyb.Pin.OUT_PP).low()
+				water.low()
+				light.low()
 				poketime = pyb.millis()		# get current time
-				starttime = poketime
 				curtime = poketime
 				while (curtime-poketime) <= iti[0]:
-					if pyb.Pin(inport, pyb.Pin.IN).value() == 0: # or pyb.Pin(inport_2, pyb.Pin.IN).value() == 0:
+					if poke.value() == 0:
 						poketime = pyb.millis()
 					curtime = pyb.millis()
-				totaltime = curtime - starttime
+				totaltime = time5 - time2
 				print('Trial '+str(i)+' of '+str(trials)+' completed. Last trial duration was '+str(totaltime)+'ms.  The iti was '+str(iti[0]))
 				i +=1
-			if (time1-time2) >= resptime[0]:
-				#pyb.Pin('Y6', pyb.Pin.OUT_PP).low()
-				#pyb.Pin('Y7', pyb.Pin.OUT_PP).low()
-				pyb.Pin('X9', pyb.Pin.OUT_PP).low()
-				pyb.delay(10000)
-				nopoke +=1
+				
+		elif i <= (trials/2):
+			time1 = pyb.millis()
+			if i - ii >= 1.0:
+				ii = i
+				time2 = pyb.millis()
+				light.high()
+				pyb.delay(5)
+			if poke.value() == 0:
+				time3 = pyb.millis()
+				time4 = pyb.millis()
+				time5 = pyb.millis()
+				while (time4 - time3) < outtime[0]:
+					if poke.value() == 0:
+						time3 = pyb.millis()
+					time4 = pyb.millis()
+				water.high()
+				pyb.delay(opentime)
+				water.low()
+				light.low()
 				poketime = pyb.millis()		# get current time
-				starttime = poketime
 				curtime = poketime
-				while (curtime-poketime) <= iti[0]:
-					if pyb.Pin(inport, pyb.Pin.IN).value() == 0: # or pyb.Pin(inport_2, pyb.Pin.IN).value() == 0:
+				while (curtime-poketime) <= iti[1]:
+					if poke.value() == 0:
 						poketime = pyb.millis()
 					curtime = pyb.millis()
-				print('Trial '+str(i)+' of '+str(trials)+' completed. Last trial had no poke.  There have been '+str(nopoke)+' no-poke trials thus far.')
+				totaltime = time5 - time2
+				print('Trial '+str(i)+' of '+str(trials)+' completed. Last trial duration was '+str(totaltime)+'ms.  The iti was '+str(iti[1]))
 				i +=1
-				
+			
 		else:
 			time1 = pyb.millis()
 			if i - ii >= 1.0:
 				ii = i
 				time2 = pyb.millis()
-				pyb.Pin('X9', pyb.Pin.OUT_PP).high()
-			if pyb.Pin(inport, pyb.Pin.IN).value() == 0: #or pyb.Pin(inport_2, pyb.Pin.IN).value() == 0:
-				pyb.Pin(outport, pyb.Pin.OUT_PP).high()
+				light.high()
+				pyb.delay(5)
+			if poke.value() == 0:
+				time3 = pyb.millis()
+				time4 = pyb.millis()
+				time5 = pyb.millis()
+				while (time4 - time3) < outtime[1]:
+					if poke.value() == 0:
+						time3 = pyb.millis()
+					time4 = pyb.millis()
+				water.high()
 				pyb.delay(opentime)
-				pyb.Pin(outport, pyb.Pin.OUT_PP).low()
-				pyb.Pin('X9', pyb.Pin.OUT_PP).low()
+				water.low()
+				light.low()
 				poketime = pyb.millis()		# get current time
-				starttime = poketime
 				curtime = poketime
-				while (curtime-poketime) <= iti[1]:
-					if pyb.Pin(inport, pyb.Pin.IN).value() == 0: #or pyb.Pin(inport_2, pyb.Pin.IN).value() == 0:
+				while (curtime-poketime) <= iti[2]:
+					if poke.value() == 0:
 						poketime = pyb.millis()
 					curtime = pyb.millis()
-				totaltime = curtime - starttime
-				print('Trial '+str(i)+' of '+str(trials)+' completed. Last trial duration was '+str(totaltime)+'ms.  The iti was '+str(iti[1]))
+				totaltime = time5 - time2
+				print('Trial '+str(i)+' of '+str(trials)+' completed. Last trial duration was '+str(totaltime)+'ms.  The iti was '+str(iti[2]))
 				i +=1
-			if (time1-time2) >= resptime[1]:
-				pyb.Pin('X9', pyb.Pin.OUT_PP).low()
-				pyb.delay(10000)
-				nopoke +=1
-				poketime = pyb.millis()		# get current time
-				starttime = poketime
-				curtime = poketime
-				while (curtime-poketime) <= iti[1]:
-					if pyb.Pin(inport, pyb.Pin.IN).value() == 0: #or pyb.Pin(inport_2, pyb.Pin.IN).value() == 0:
-						poketime = pyb.millis()
-					curtime = pyb.millis()
-				print('Trial '+str(i)+' of '+str(trials)+' completed. Last trial had no poke.  There have been '+str(nopoke)+' no-poke trials thus far.')
-				i +=1
-
+			
 	print('Shrek says: It\'s all ogre now.')
 
+# Basic nose poke random time with 1 pokes
 
 def basic_rand(outport = 'Y2', opentime = 13, pokeport = 'X8', trials = 100, iti = [1000, 2000,12000], outtime = [250,250]):
 	i = 1			# trial counter
@@ -139,37 +169,69 @@ def basic_rand(outport = 'Y2', opentime = 13, pokeport = 'X8', trials = 100, iti
 	poke = pyb.Pin(pokeport, pyb.Pin.IN, pyb.Pin.PULL_UP)
 	pyb.delay(10000)
 	while i <= trials:
-		time1 = pyb.millis()
-		if i - ii >= 1.0:
-			ii = i
-			time2 = pyb.millis()
-			light.high()
-			pyb.delay(5)
-		if poke.value() == 0:
-			time3 = pyb.millis()
-			time4 = pyb.millis()
-			time5 = pyb.millis()
-			while (time4 - time3) < outtime[0]:
-				if poke.value() == 0:
-					time3 = pyb.millis()
+		if i <= 5:
+			time1 = pyb.millis()
+			if i - ii >= 1.0:
+				ii = i
+				time2 = pyb.millis()
+				light.high()
+				pyb.delay(5)
+			if poke.value() == 0:
+				time3 = pyb.millis()
 				time4 = pyb.millis()
-			water.high()
-			pyb.delay(opentime)
-			water.low()
-			light.low()
-			poketime = pyb.millis()		# get current time
-			curtime = poketime
-			rand = pyb.rng()*(1.0/(2**30-1))
-			trial_iti = math.floor(rand*(iti[2]-iti[1])+iti[1])
-			while (curtime-poketime) <= trial_iti:
-				if poke.value() == 0:
-					poketime = pyb.millis()
-				curtime = pyb.millis()
-			totaltime = time5 - time2
-			print('Trial '+str(i)+' of '+str(trials)+' completed. Last trial duration was '+str(totaltime)+'ms.  The iti was '+str(trial_iti))
-			i +=1
+				time5 = pyb.millis()
+				while (time4 - time3) < outtime[0]:
+					if poke.value() == 0:
+						time3 = pyb.millis()
+					time4 = pyb.millis()
+				water.high()
+				pyb.delay(opentime)
+				water.low()
+				light.low()
+				poketime = pyb.millis()		# get current time
+				curtime = poketime
+				while (curtime-poketime) <= iti[0]:
+					if poke.value() == 0:
+						poketime = pyb.millis()
+					curtime = pyb.millis()
+				totaltime = time5 - time2
+				print('Trial '+str(i)+' of '+str(trials)+' completed. Last trial duration was '+str(totaltime)+'ms.  The iti was '+str(iti[0]))
+				i +=1
+				
+		elif i <= (trials):
+			time1 = pyb.millis()
+			if i - ii >= 1.0:
+				ii = i
+				time2 = pyb.millis()
+				light.high()
+				pyb.delay(5)
+			if poke.value() == 0:
+				time3 = pyb.millis()
+				time4 = pyb.millis()
+				time5 = pyb.millis()
+				while (time4 - time3) < outtime[0]:
+					if poke.value() == 0:
+						time3 = pyb.millis()
+					time4 = pyb.millis()
+				water.high()
+				pyb.delay(opentime)
+				water.low()
+				light.low()
+				poketime = pyb.millis()		# get current time
+				curtime = poketime
+				rand = pyb.rng()*(1.0/(2**30-1))
+				trial_iti = math.floor(rand*(iti[2]-iti[1])+iti[1])
+				while (curtime-poketime) <= trial_iti:
+					if poke.value() == 0:
+						poketime = pyb.millis()
+					curtime = pyb.millis()
+				totaltime = time5 - time2
+				print('Trial '+str(i)+' of '+str(trials)+' completed. Last trial duration was '+str(totaltime)+'ms.  The iti was '+str(trial_iti))
+				i +=1
 			
 	print('Shrek says: It\'s all ogre now.')
+	
+# Training for go-no-go tasks
 	
 def gng_train(outports = ['Y1', 'Y2', 'Y3', 'Y4'], opentimes = [13, 13, 13, 13], pokeport = 'X8', trials = 100, iti = [13000, 16000], outtime = [500,500], trialdur = 30000, training = 'go'):
     # training can be 'go', 'nogo', or 'gonogo'
