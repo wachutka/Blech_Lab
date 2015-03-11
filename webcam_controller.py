d65dfe153a7f23317fa0b170403b59688258c672
narendramukherjee <narendramukherjee@users.noreply.github.com>
2015-03-11 17:00:56 -0400

    webcam_controller.py
    One input starts off the webcam 1 second before the valves are triggered, and the video files are renamed according to the valve input that arrives later

--- webcam_controller.py
+++ webcam_controller.py
@@ -1,14 +1,17 @@
-import time
+import datetime
 import os
 import RPi.GPIO as GPIO
 import easygui
+import numpy as np
 
 # List of ports to be listened to for inputs
 inports = ['', '', '', '']
+tastes = ['sucrose', 'quinine', 'water', 'nonsense']
+video_input = 35 #Integer
 
 # Set the port in inports to inputs
 for inport in inports:
-	GPIO.setup(inport, GPIO.IN)
+	GPIO.setup(int(inport), GPIO.IN)
 
 # Ask the user for the directory to save the videos in
 directory = easygui.diropenbox(msg = 'Select the directory to save the videos from this experiment', title = 'Select directory')
@@ -18,27 +21,32 @@ os.chdir(directory)
 # Counter of trials
 trials = [1, 1, 1, 1]
 
-# Run an infinite loop and wait for inputs. Save an appropriately named video for 5 seconds if an input comes in
+# Run an infinite loop and wait for the video input. Save a generically named video for 6 seconds if an input comes in
 while True:
-	if GPIO.input(inports[0]) == 1:
-		filename = 'port' + inports[0] + '_trial' + str(trials[0]) + '.avi'  
-		os.system('sudo streamer -q -c /dev/video0 -s 640x480 -f jpeg -t 150 -r 30 -j 75 -w 0 -o %s' %filename)
-		trials[0] += 1
-
-	if GPIO.input(inports[1]) == 1:
-		filename = 'port' + inports[1] + '_trial' + str(trials[1]) + '.avi'  
-		os.system('sudo streamer -q -c /dev/video0 -s 640x480 -f jpeg -t 150 -r 30 -j 75 -w 0 -o %s' %filename)
-		trials[1] += 1
-
-	if GPIO.input(inports[2]) == 1:
-		filename = 'port' + inports[2] + '_trial' + str(trials[2]) + '.avi'  
-		os.system('sudo streamer -q -c /dev/video0 -s 640x480 -f jpeg -t 150 -r 30 -j 75 -w 0 -o %s' %filename)
-		trials[2] += 1
-
-	if GPIO.input(inports[3]) == 1:
-		filename = 'port' + inports[3] + '_trial' + str(trials[3]) + '.avi'  
-		os.system('sudo streamer -q -c /dev/video0 -s 640x480 -f jpeg -t 150 -r 30 -j 75 -w 0 -o %s' %filename)
-		trials[3] += 1
+	if GPIO.input(video_input) == 1:
+		os.system('sudo streamer -q -c /dev/video0 -s 640x480 -f jpeg -t 360 -r 60 -j 75 -w 0 -o generic.avi')
+		start = datetime.datetime.now()
+		current = datetime.datetime.now()
+		delta = current - start
+		input_port = None
+		while np.abs(delta.total_seconds()) <= 7.0:
+			if GPIO.input(int(inports[0])) == 1:
+				input_port = 0
+			if GPIO.input(int(inports[1])) == 1:
+				input_port = 1
+			if GPIO.input(int(inports[2])) == 1:
+				input_port = 2
+			if GPIO.input(int(inports[3])) == 1:
+				input_port = 3
+			#Update the current time
+			current = datetime.datetime.now()
+			delta = current - start
+				
+		#rename the generic.avi file according to the input received
+		os.system('mv generic.avi ' + tastes[input_port] + '_trial_' + str(trials[input_port]) + '.avi')
+		
+		#increment the appropriate counter of trials
+		trials[input_port] += 1
 
 # Doesn't really matter as forced exit is needed out of this program
 GPIO.cleanup()
